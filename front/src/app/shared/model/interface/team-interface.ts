08de9db453c3b1bb1412ad4f903994c14c39976f
Angelo <angeloochirat@gmail.com>
2019-05-26 18:19:31 +0200

    Squelette team service front

---
+++ front/src/app/shared/model/interface/team-interface.ts
@@ -0,0 +1,3 @@
+export interface TeamInterface {
+    name: string;
+}
--- front/src/app/shared/team/team.service.ts
+++ front/src/app/shared/team/team.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {TeamInterface} from '../model/interface/team-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createTeam(team: TeamInterface): Observable<any> {
+    return this.http.post<any>('http://127.0.0.1:8088/api/team/all', team);
+  }
+
+  getTeam(id: string): Observable<TeamInterface> {
+    return this.http.get<TeamInterface>('http://127.0.0.1:8088/api/team/' + id);
+  }
+
+  getTeams(): Observable<TeamInterface> {
+    return this.http.get<TeamInterface>('http://127.0.0.1:8088/api/team/all');
+  }
+
+  deleteTeam(id: string): Observable<any> {
+    return this.http.delete<any>('http://127.0.0.1:8088/api/team/delete/' + id);
+  }
 }
