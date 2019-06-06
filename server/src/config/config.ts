bdfeaee8653eacd957bcee2e90922d0cc3231ef1
Driss AHMED BACHA <driss.ahmedbacha@gmail.com>
2019-06-06 14:15:37 +0200

    Ajout cross-origin

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,6 +1,7 @@
 import {Configuration} from 'tsunamy/core';
 
 export let CONFIGURATION: Configuration = {
+  allowOrigin: ['http://localhost:4200'],
   hostname : '127.0.0.1',
   http: true,
   httpPort: 8088,
